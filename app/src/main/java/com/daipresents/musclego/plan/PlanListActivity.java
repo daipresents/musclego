1aaac44637e2bff7edc33cf0beb194bc59e5a0f8
Dai Fujihara <Dai Fujihara>
2016-11-10 22:19:47 +0900

    Initial commit

---
+++ app/src/main/java/com/daipresents/musclego/plan/PlanListActivity.java
@@ -0,0 +1,13 @@
+package com.daipresents.musclego.plan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlanListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan_list);
+    }
+}
